26ca5c88fcbc92a21c10d4b46ac7125dd41b1414
choniyuazwan <choniyuazwan@gmail.com>
2018-03-29 08:40:39 +0700

    part 2 @ Thu, Mar 29, 2018  8:40:38 AM

--- application/views/p_login.php
+++ application/views/p_login.php
@@ -25,20 +25,29 @@
             </div>
             <!-- /.box-header -->
             <!-- form start -->
-            <form class="form-horizontal">
+            <?php
+              if(isset($_POST['masuk'])) {
+                $u = $this->input->post['usr'];
+                $p = $this->input->post['pwd'];
+                if($u == 'admin' && $p == 'admin') {
+                  header('location:'.base_url().'admin');
+                } else if($u == 'user' && $p == 'user') {
+                  header('location:'.base_url().'user');
+                }
+              }
+            ?>
+            <form class="form-horizontal" method="post" action="">
               <div class="box-body">
                 <div class="form-group">
                   <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
-
                   <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    <input type="text" class="form-control" name="usr" placeholder="Email">
                   </div>
                 </div>
                 <div class="form-group">
                   <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-
                   <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    <input type="text" class="form-control" name="pwd" placeholder="Password">
                   </div>
                 </div>
                 <div class="form-group">
@@ -53,8 +62,8 @@
               </div>
               <!-- /.box-body -->
               <div class="box-footer">
-                <button type="submit" class="btn btn-default">Cancel</button>
-                <button type="submit" class="btn btn-info pull-right">Sign in</button>
+                <button type="reset" class="btn btn-default">Cancel</button>
+                <button type="submit" name="masuk" class="btn btn-info pull-right">Sign in</button>
               </div>
               <!-- /.box-footer -->
             </form>
